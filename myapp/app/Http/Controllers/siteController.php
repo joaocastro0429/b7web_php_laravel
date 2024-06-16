48640afffe9bdd17cccaf0fcb7ae441ed130fd0c
joaocastro <joaocastro0429@gmail.com>
2024-06-15 23:17:00 -0300

    apredendo laravel

---
+++ myapp/app/Http/Controllers/siteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class siteController extends Controller
+{
+    public function index(){
+        return view('bemvindo');
+    }
+
+    public function tchau(){
+        return view("sair");
+    }
+}
